ef8d36e94bfc746f3795f323803b040051e12bfe
cidaluna <cidalunna@hotmail.com>
2022-10-25 22:31:11 -0300

    Calculating meters, centimeters, millimeters

---
+++ 09_distancia_metros.py
@@ -0,0 +1,18 @@
+metros = float(input('Digite uma distância em metros: '))
+
+km = metros / 1000
+hm = metros / 100
+dam = metros / 10
+m = metros
+dm = metros * 10
+cm = metros * 100
+mm = metros * 1000
+
+print('A medida de {} metros, corresponde a: '.format(m))
+print('km: {} '.format(km))
+print('hm: {}'.format(hm))
+print('dam: {}'.format(dam))
+print('m: {}'.format(m))
+print('dm: {}'.format(dm))
+print('cm: {}'.format(cm))
+print('mm: {}'.format(mm))
