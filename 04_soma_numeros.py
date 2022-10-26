b4d658f59e71586cd77dace477327d02194658db
cidaluna <cidalunna@hotmail.com>
2022-10-25 21:37:59 -0300

    Sum of integers

---
+++ 04_soma_numeros.py
@@ -0,0 +1,6 @@
+numero1 = int(input('Digite o primeiro número inteiro: '))
+numero2 = int(input('Digite o segundo número inteiro: '))
+
+soma = numero1 + numero2
+
+print('Somando {} + {} temos {}' .format(numero1, numero2, soma))
