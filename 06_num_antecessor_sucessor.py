85faf075629b89a92d74d5aff176349163cc96eb
cidaluna <cidalunna@hotmail.com>
2022-10-25 22:00:54 -0300

    Predecessor and sucessor number

---
+++ 06_num_antecessor_sucessor.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um numero: '))
+antecessor = num - 1
+sucessor = num + 1
+
+print('Você digitou o número {}: seu antecessor é {} e o seu sucessor é {}.' .format(
+    num, antecessor, sucessor))
