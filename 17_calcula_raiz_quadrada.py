a7dacaf7797715ea12d700107257d6e56924b33b
cidaluna <cidalunna@hotmail.com>
2022-10-27 07:59:25 -0300

    Calculate square root

---
+++ 17_calcula_raiz_quadrada.py
@@ -0,0 +1,9 @@
+
+num = int(input('Entre com um número: '))
+
+calculaRaizQuadradaJeitoA = num ** (1/2)
+
+calculaRaizQuadradaJeitoB = num ** 0.5
+
+print('A raiz quadrada de {} é {}'.format(num, calculaRaizQuadradaJeitoA))
+print('A raiz quadrada de {} é {}'.format(num, calculaRaizQuadradaJeitoB))
